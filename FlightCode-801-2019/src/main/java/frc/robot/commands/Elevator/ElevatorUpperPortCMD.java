2409de35ea26450f5e38432b205dea4549152c5c
Glenn <texasbubb@gmail.com>
2019-03-12 22:30:11 -0400

    Elevator Inititial checkin.

--- FlightCode-801-2019/src/main/java/frc/robot/commands/Elevator/ElevatorUpperPortCMD.java
+++ FlightCode-801-2019/src/main/java/frc/robot/commands/Elevator/ElevatorUpperPortCMD.java
@@ -19,24 +19,27 @@ public class ElevatorUpperPortCMD extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-
+    Robot.elevator.updatePID();
+    Robot.elevator.updateSmartMotion();
+    Robot.elevator.carriageRun();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.elevator.carriageEncoderPos();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return !Robot.elevator.carriageIsMoving();
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
-
+    Robot.elevator.stop();
   }
 
   // Called when another command which requires one or more of the same
--- FlightCode-801-2019/src/main/java/frc/robot/commands/Lift/LiftUpDownToggleCMD.java
+++ FlightCode-801-2019/src/main/java/frc/robot/commands/Lift/LiftUpDownToggleCMD.java
@@ -34,7 +34,7 @@ public class LiftUpDownToggleCMD extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return Robot.lift.isMoving();
+    return Robot.lift.isDeltaPosition();
   }
 
   // Called once after isFinished returns true
